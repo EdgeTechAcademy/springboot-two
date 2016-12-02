dfd7329f772b96653a468fd47b83c152297fc21c
gtjames <gjames@ancoraeducation.com>
2016-12-02 11:14:33 -0600

    first commit

--- src/main/java/edgetech/springframework/repositories/ProductRepository.java
+++ src/main/java/edgetech/springframework/repositories/ProductRepository.java
@@ -3,5 +3,8 @@ package edgetech.springframework.repositories;
 import edgetech.springframework.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Created by Edge Tech Academy on 11/22/2016.
+ */
 public interface ProductRepository extends CrudRepository<Product, Integer>{
 }
