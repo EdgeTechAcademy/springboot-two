e4d0b7f7e7dae84d3f1ffc5ba33346549a9df018
gtjames <gjames@ancoraeducation.com>
2017-11-06 06:50:30 -0600

    remove the test modules

--- src/main/java/edgetech/springframework/SpringBootWebApplication.java
+++ src/main/java/edgetech/springframework/SpringBootWebApplication.java
@@ -2,10 +2,6 @@ package edgetech.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-//import org.springframework.boot.context.embedded.ErrorPage;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.HttpStatus;
 
 @SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class SpringBootWebApplication {
